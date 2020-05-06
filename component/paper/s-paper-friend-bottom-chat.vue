51102133f680b4b8b9baf3d3f4ff706d3b8784aa
shj <425198567@qq.com>
2020-05-06 22:21:36 +0800

    小纸条左边聊天框

---
+++ component/paper/s-paper-friend-bottom-chat.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="bottom_input_wrap">
+		<input type="text" placeholder="文明发言" v-model="inputText" />
+		<view class="iconfont icon-fabu" @click="handleSendChat">
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				inputText: ""
+			}
+		},
+		methods: {
+			handleSendChat() {
+				this.$emit('sendChat', this.inputText)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.bottom_input_wrap {
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		padding: 20rpx 0;
+		border-top: 1rpx solid #EEEEEE;
+		display: flex;
+
+		input {
+			margin-left: 20rpx;
+			flex: 1;
+			background-color: #f7f7f7;
+			border-radius: 10rpx;
+			font-size: 30rpx;
+			padding: 10rpx;
+		}
+
+		.icon-fabu {
+			width: 100rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+</style>
--- pages/paper/s-paper-chat/s-paper-chat.vue
+++ pages/paper/s-paper-chat/s-paper-chat.vue
@@ -1,51 +1,64 @@
 <template>
 	<view>
-		<view class="bottom_input_wrap">
-			<input type="text" placeholder="文明发言" />
-			<view class="iconfont icon-fabu">
-
+		<view class="chat_left_wrap">
+			<image src="../../../static/userpic/11.jpg" mode="widthFix" lazy-load></image>
+			<view class="chat_left_msg">
+				上的飞机卢卡斯的积分看来就是打开链接发士大夫记录卡撒旦艰苦放了几颗螺丝
+				<view class="chat_left_msg_arrow">
+					
+				</view>
 			</view>
 		</view>
+		<s-paper-friend-bottom-chat @sendChat="handleSendChat"></s-paper-friend-bottom-chat>
 	</view>
 </template>
 
 <script>
+	import sPaperFriendBottomChat from "../../../component/paper/s-paper-friend-bottom-chat.vue"
 	export default {
+		components: {
+			sPaperFriendBottomChat
+		},
 		data() {
 			return {
 
 			}
 		},
 		methods: {
-
+			handleSendChat(value) {
+				console.log(value)
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.bottom_input_wrap {
-		width: 100%;
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		padding: 20rpx 0;
-		border-top: 1rpx solid #EEEEEE;
+	.chat_left_wrap{
+		margin-left: 20rpx;
 		display: flex;
-
-		input {
-			margin-left: 20rpx;
-			flex: 1;
-			background-color: #f7f7f7;
-			border-radius: 10rpx;
-			font-size: 30rpx;
-			padding: 10rpx;
-		}
-
-		.icon-fabu {
+		image{
 			width: 100rpx;
+			height: 100rpx;
+			border-radius: 50%;
+			flex-shrink: 0;
+		}
+		.chat_left_msg{
 			display: flex;
-			justify-content: center;
 			align-items: center;
+			margin-left: 20rpx;
+			background-color: #f4f4f4;
+			position: relative;
+			padding: 0 15rpx;
+			border-radius: 10rpx;
+			margin-right: 120rpx;
+			.chat_left_msg_arrow::after{
+				position: absolute;
+				top: 25rpx;
+				left: -30rpx;
+				content: "";
+				border: 15rpx solid #f4f4f4;
+				border-color: transparent #f4f4f4 transparent transparent;
+			}
 		}
 	}
 </style>
